753cadf8115db8b58e209b5480c521264a252da0
rhayk <rhayk@10.7.26.27>
2019-09-20 21:18:02 -0300

    Inicio do projeto calendário

---
+++ ProjetoData/src/Calendario.java
@@ -0,0 +1,39 @@
+
+public class Calendario {
+private byte dia;
+private byte mes;
+private int ano;
+
+void Calendario (byte dia, byte mes, int ano) {
+	this.dia = dia;
+	this.mes = mes;
+	this.ano = ano;
+	}
+
+public byte getDia() {
+	return dia;
+}
+
+public void setDia(byte dia) {
+	this.dia = dia;
+} 
+
+public byte getMes() {
+	return mes;
+}
+
+public void setMes(byte mes) {
+	this.mes = mes;
+}
+
+public int getAno() {
+	return ano;
+}
+
+public void setAno(int ano) {
+	this.ano = ano;
+}
+																					
+
+
+}
